0e036200da7819484691cba1d220e681de15a222
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-17 18:37:24 +0200

    Game controlling class

--- snake/game.py
+++ snake/game.py
@@ -2,8 +2,42 @@
 import pygame
 
 
-def draw():
-    pass
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = (640, 400)
+    
+    def on_init(self) -> None:
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+    
+    def on_event(self, event) -> None:
+        if event.type == pygame.QUIT:
+            self._running = False
+    
+    def on_loop(self) -> None:
+        pass
 
-def main():
-    pass
+    def on_render(self) -> None:
+        pass
+
+    def on_cleanup(self) -> None:
+        pygame.quit()
+
+    def on_execute(self) -> None:
+        if self.on_init() is False:
+            self._running = False
+
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup() 
+
+
+if __name__ == "__main__":
+    app = App()
+    app.on_execute()

e4844a6d4315bd9939cf3eb35a75c4a132b58133
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-19 20:52:26 +0200

    Fix node component

--- app/node.py
+++ app/node.py
@@ -4,17 +4,20 @@ from app.utils import Direction
 
 
 class Node:
-    def __init__(self, x: int, y: int, direction: Direction, next: Optional[Node] = None):
+
+    size = 10
+
+    def __init__(self, x: int, y: int, direction: Direction, next_node: Optional[Node] = None):
         self.x = x
-        self.y = x
+        self.y = y
         self.direction = direction
-        self._next = next
+        self._next_node = next_node
 
     @property
-    def next(self) -> Node:
-        return self._next
+    def next_node(self) -> Node:
+        return self._next_node
 
-    @next.setter
-    def next(self, value: Node) -> None:
+    @next_node.setter
+    def next_node(self, value: Node) -> None:
         if isinstance(value, Node):
-            self._next = value
+            self._next_node = value

1d2bae8b5530e86623e1bdafc070cdcab06a6222
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-20 20:09:18 +0200

    logic in snake class

--- app/snake.py
+++ app/snake.py
@@ -14,7 +14,11 @@ class Snake:
 
     @property
     def head(self) -> Node:
-        return self._nodes[0]
+        return self.nodes[0]
+
+    @property
+    def tail(self) -> Node:
+        return self.nodes[0]
 
     @property
     def speed(self) -> int:
@@ -29,15 +33,26 @@ class Snake:
         return self.alive
 
     def turn(self, direction: Direction):
-        if (direction == Direction.left and self.head.direction == Direction.right or
-                direction == Direction.right and self.head.direction == Direction.left or
-                direction == Direction.up and self.head.direction == Direction.down or
-                direction == Direction.down and self.head.direction == Direction.up):
+        if direction == self.head.direction:
             return
+
+        current_direction = self.head.direction.value
+        if (direction.value == 'LEFT' and current_direction == 'RIGHT' or
+                direction.value == 'RIGHT' and current_direction == 'LEFT' or
+                direction.value == 'UP' and current_direction == 'DOWN' or
+                direction.value == 'DOWN' and current_direction == 'UP'):
+            return
+        print("Turn {}".format(direction.value))
         self._turns.append(Node(self.head.x, self.head.y, direction))
 
     def move(self):
         for node in self.nodes:
+            for turn in self._turns:
+                if node.x == turn.x and node.y == node.y:
+                    node.direction = turn.direction
+                    if node == self.tail:
+                        self._turns.remove(turn)
+
             direction = node.direction.value
             if direction == 'UP':
                 node.y -= self.speed
@@ -48,5 +63,12 @@ class Snake:
             elif direction == 'RIGHT':
                 node.x += self.speed
 
+        if self.check_collision():
+            self.alive = False
+
     def check_collision(self) -> bool:
-        pass
+        x, y = self.head.x, self.head.y
+        for node in self.nodes[1:]:
+            if x == node.x and y == node.y:
+                return True
+        return False

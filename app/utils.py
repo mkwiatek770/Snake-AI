97305d3af724aae942a4b7ab489586bea90272d5
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-24 21:29:37 +0200

    add speed enum structure

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,16 @@ from enum import Enum
 
 NODE_SIZE = 10
 
+
 class Direction(Enum):
     up = 'UP'
     down = 'DOWN'
     left = 'LEFT'
     right = 'RIGHT'
+
+
+class Speed(Enum):
+    debug = int(NODE_SIZE * 0.1)
+    slow = int(NODE_SIZE * 2)
+    normal = int(NODE_SIZE * 5)
+    fast = int(NODE_SIZE * 1)
